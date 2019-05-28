13f21848b4dd2ba74833861bb2c76157e8fd6f20
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-05-28 15:35:52 -0400

    adding header and footer components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import Login from './pages/Login';
@@ -7,10 +7,14 @@ import Welcome from './pages/Welcome';
 import Todos from './pages/Todos';
 import RouteNotFound from './pages/RouteNotFound';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route path="/" exact component={Login} />
           <Route path="/login" component={Login} />
@@ -18,6 +22,7 @@ function App() {
           <Route path="/todos" component={Todos} />
           <Route path="" component={RouteNotFound} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
