01007eb2e90127187cc92348a998a96b726624c8
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-05-28 15:56:01 -0400

    adding code to ensure we can skip header on / and .login page. keeping code commented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react'
+//import { withRouter } from 'react-router-dom'
 
 class Header extends Component {
+    // to see header except on / and /login, uncomment all the commented out lines of code
+    // https://stackoverflow.com/questions/42010053/react-router-this-props-location ( search for word withRouter )
     render() {
         return (
             <div>
-                Header<hr/>
+                {/*((this.props.location.pathname !== '/') && (this.props.location.pathname !== '/login')) && <div>Header<hr/></div>*/}
+                Header<hr />
             </div>
         )
     }
 }
 
+//export default withRouter(Header)
 export default Header
