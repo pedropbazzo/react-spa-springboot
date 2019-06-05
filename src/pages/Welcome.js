2e34bb96e9a3400312e839ddc8d52af418f11dbf
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-06-04 21:12:29 -0400

    getting axios

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,11 +2,16 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom';
 
 class Welcome extends Component {
+    constructor(props) {
+        super(props)
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+    }
     render() {
         return (
             <div className="container">
                 <h1>Welcome!</h1>
                 <div>Welcome john.doe. You can manage your Todos at <Link to="/todos">Todos</Link></div>
+                <div>Click <button onClick={this.retrieveWelcomeMessage}>Get welcome message</button> to get a customized welcome message</div>
             </div>
         )
     }
