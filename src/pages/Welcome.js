d3e73bf15e74d18be9c8ebcfd20060ab6d42ee2f
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-06-04 22:31:57 -0400

    getting data from Springboot REST

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,20 +1,38 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom';
+import ApiService from '../services/ApiService'
 
 class Welcome extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            welcomeMessage: ''
+        }
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
     }
+    
     render() {
         return (
             <div className="container">
                 <h1>Welcome!</h1>
                 <div>Welcome john.doe. You can manage your Todos at <Link to="/todos">Todos</Link></div>
                 <div>Click <button onClick={this.retrieveWelcomeMessage}>Get welcome message</button> to get a customized welcome message</div>
+                <h3>{this.state.welcomeMessage}</h3>
             </div>
         )
     }
+
+    retrieveWelcomeMessage() {
+        let timeStamp = Math.floor(Date.now());
+        ApiService.get('http://localhost:8082/hello-world/' + timeStamp)
+        .then(response => {
+            this.setState({
+                welcomeMessage: response.data.message
+            })
+        })
+        .catch(error => console.log(error))
+    }
+
 }
 
 export default Welcome
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,5 +1,9 @@
-class ApiService {
+import axios from 'axios'
 
+class ApiService {
+    get(url) {
+       return axios.get(url)
+    }
 }
 
 export default new ApiService()
