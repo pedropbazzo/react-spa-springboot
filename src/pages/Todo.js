5aadd23007bdcec2d93643125c2c3fb96c387c0b
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-07-08 16:54:22 -0400

    basic validation using Formik

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,32 +1,61 @@
 import React, { Component } from 'react'
 import moment from 'moment'
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 class Todo extends Component {
     constructor(props) {
         super(props)
         this.state = {
             id: this.props.match.params.id,
-            description: 'Learn Forms',
+            description: 'Lear',
             targetDate: moment(new Date()).format('YYYY-MM-DD')
         }
+        this.onSubmit = this.onSubmit.bind(this)
+        this.validate = this.validate.bind(this)
+    }
+
+    onSubmit(values) {
+        console.log(values)
+    }
+
+    validate(values) {
+        let errors = {}
+        if(!values.description.trim()) {
+            errors.description = 'Enter a description'
+        } else if(values.description.trim().length < 5) {
+            errors.description = 'Description should contain atleast 5 characters'
+        }
+
+        if(!moment(values.targetDate).isValid()) {
+            errors.targetDate = 'Enter a valid Target Date'
+        }
+        return errors
     }
     
     render() {
+        let { description, targetDate } = this.state
         return (
             <div className="container">
                 <h2>Todo</h2>
-                <Formik>
+                <Formik 
+                    initialValues={{description, targetDate}} 
+                    onSubmit={this.onSubmit} 
+                    validate = {this.validate}
+                    validateOnChange={false}
+                    validateOnBlur={false}
+                >
                     {
                         (props) => (
                             <Form>
                                 <fieldset className="form-group">
                                     <label>Description</label>
                                     <Field className="form-control" type="text" name="description" />
+                                    <ErrorMessage name="description" component="div" className="text-danger" />
                                 </fieldset>
                                 <fieldset className="form-group">
                                     <label>Target Date</label>
                                     <Field className="form-control" type="date" name="targetDate" />
+                                    <ErrorMessage name="targetDate" component="div" className="text-danger" />
                                 </fieldset>
                                 <button className="btn btn-success" type="submit">Save</button>
                             </Form>
